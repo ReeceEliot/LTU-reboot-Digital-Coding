b349374790cdb8a36834d69bf915ea42f6b73d57
ReeceEliot <72089641+ReeceEliot@users.noreply.github.com>
2020-10-12 11:35:04 +0100

    add to number variables together

---
+++ module-2/module-3/scripts/main.js
@@ -0,0 +1,16 @@
+
+var numberOfSmarties = 40;
+
+var numberOfIceCreams = 30;
+
+var total = numberOfSmarties + numberOfIceCreams; // 40 + 30 = 70
+
+console.log ( total ); // 70
+
+/*
++ add
+- subtract
+/ divide
+* multiply
+% modulus
+*/
